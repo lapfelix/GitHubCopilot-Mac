15a9d52640dd10be4eacea1b4866f45467e8d17f
lapfelix <felix@lapal.me>
2025-01-16 15:59:07 -0500

    Initial Commit

---
+++ GitHub Copilot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitHub Copilot
+//
+//  Created by Felix Lapalme on 2025-01-16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
