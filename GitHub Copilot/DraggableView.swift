0c64abd8a854f32a68f44b35bc987b69290f2289
lapfelix <felix@lapal.me>
2025-02-09 22:22:28 -0500

    Double clicking the header should maximize

--- GitHub Copilot/DraggableView.swift
+++ GitHub Copilot/DraggableView.swift
@@ -10,6 +10,10 @@ import Cocoa
 /** A view that can be used to drag the window around. */
 class DraggableView: NSView {
     override func mouseDown(with event: NSEvent) {
-        self.window?.performDrag(with: event)
+        if event.clickCount == 2 {
+            self.window?.performZoom(nil) // maximize the window on double-click
+        } else {
+            self.window?.performDrag(with: event)
+        }
     }
 }
